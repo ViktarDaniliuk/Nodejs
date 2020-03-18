8d124f5892e7459c94b48a37fe8eeb2b314d3fc0
ViktarDaniliuk <daniliukviktar@gmail.com>
2020-03-18 21:29:46 +0100

    feat. added delete folders

--- sorter.js
+++ sorter.js
@@ -1,10 +1,30 @@
 const fs = require('fs');
 
-const moveFile = (path, newFolderName, file) => {
+const moveFile = (path, newFolderName, file, index) => {
    fs.rename(path, `${newFolderName}/${file}`, err => {
       if (err) throw err;
+
+      
    });
 }
+
+const deleteFolders = (path) => {
+   fs.readdir(`${__dirname}/${path}`, (err, data) => {
+      if (err) throw err;
+
+      if (data.length === 0) {
+         fs.rmdir(`${__dirname}/${path}`, err => {
+            if (err) throw err;
+         });
+      } else {
+         data.forEach(item => {
+            const nextLevelPath = `${path}/${item}`;
+
+            deleteFolders(nextLevelPath);
+         })
+      };
+   });
+};
 
 const sorter = (path, newPath) => {
    const newFolders = new Set();
@@ -16,7 +36,7 @@ const sorter = (path, newPath) => {
          
          fs.readdir(`${__dirname}/${path}`, (err, data) => {
             if (err) throw err;
-            
+
             data.forEach((item, i) => {
                const pathToItem = `${__dirname}/${path}/${item}`;
                const firstLetterOfTheFileName = `${__dirname}/${newPath}/${item.slice(0,1).toUpperCase()}`;
@@ -35,17 +55,61 @@ const sorter = (path, newPath) => {
                         fs.mkdir(firstLetterOfTheFileName, err => {
                            if (err) throw err;
 
-                           moveFile(pathToItem, firstLetterOfTheFileName, item);
+                           // moveFile(pathToItem, firstLetterOfTheFileName, item, i);
+                           fs.rename(pathToItem, `${firstLetterOfTheFileName}/${item}`, err => {
+                              if (err) throw err;
+                        
+                              fs.readdir(`${__dirname}/${path}`, (err, data) => {
+                                 if (err) throw err;
+
+                                 if (data.length !== 0) return;
+                                 
+                                 fs.rmdir(`${__dirname}/${path}`, err => {
+                                    if (err) throw err;
+                                 });
+                              });
+                           });
                         });
                      } else {
-                        moveFile(pathToItem, firstLetterOfTheFileName, item);
+                        // moveFile(pathToItem, firstLetterOfTheFileName, item, i);
+                        fs.rename(pathToItem, `${firstLetterOfTheFileName}/${item}`, err => {
+                           if (err) throw err;
+                           
+                           fs.readdir(`${__dirname}/${path}`, (err, data) => {
+                              if (err) throw err;
+
+                              if (data.length !== 0) return;
+                              
+                              fs.rmdir(`${__dirname}/${path}`, err => {
+                                 if (err) throw err;
+                              });
+                           });
+                        });
                      };
                   };
                });
             });
          });
-      };
+         // const deleteFolders = (path) => {
+         //    fs.readdir(`${__dirname}/${path}`, (err, data) => {
+         //       if (err) throw err;
+
+         //       if (data.length === 0) {
+         //          fs.rmdir(`${__dirname}/${path}`, err => {
+         //             if (err) throw err;
+         //          });
+         //       } else {
+         //          data.forEach(item => {
+         //             const nextLevelPath = `${path}/${item}`;
    
+         //             deleteFolders(nextLevelPath);
+         //          })
+         //       };
+         //    });
+         // };
+
+      };
+      
       readDir(path)
    });
 };
