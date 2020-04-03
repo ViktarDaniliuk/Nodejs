81520cb825756c8fd358c55e45404488da536911
ViktarDaniliuk <daniliukviktar@gmail.com>
2020-04-03 20:46:04 +0200

    refactor. deleted unnecessary files

--- controllers/admin-skills.js
+++
@@ -1,8 +0,0 @@
-module.exports.post = function (req, res) {
-   console.log(req.body);
-   const skills = require('../storage/storage')
-
-   skills.setSkills(req.body);
-
-   res.redirect(301, '../admin');
-};
--- controllers/admin.js
+++
@@ -1,7 +0,0 @@
-module.exports.get = function (req, res) {
-   const skills = require('../storage/storage');
-
-   if (req.session.isAuth) {
-      res.render('admin', { skills: skills.getSkills().skills });
-   }
-};
